a6f686aafc1ce125aecc404ef9c19f74fd353501
Malaika Karim <42036621+TypicalMalaika@users.noreply.github.com>
2020-07-10 16:17:02 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+//Variable my age
+var myAge = 20;
+//Variable early years
+var earlyYears = 2;
+earlyYears *= 10.5;
+var laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears, laterYears);
+var myAgeInDogYears = earlyYears + laterYears;
+myName = "Malaika";
+myName = myName.toLowerCase();
+console.log(myName);
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
